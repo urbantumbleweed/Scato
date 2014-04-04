eaa19e7181f8b360bd4f661de571bb148809a055
urbantumbleweeed <saunders.stephen@gmail.com>
2014-04-04 17:24:12 -0400

    added methods to the YahooFinanceWrapper model for realtime and extended 'get quote' methods

--- scato_app/app/models/yahoo_finance_wrapper.rb
+++ scato_app/app/models/yahoo_finance_wrapper.rb
@@ -1,6 +1,20 @@
 class YahooFinanceWrapper
 
+	# RubyDoc: http://rubydoc.info/gems/yahoofinance/1.2.2/frames
+	# Homepage: www.transparentech.com/projects/yahoofinance
 
-	
+	#retrieves realtime quote information.
+	#can enter multiple tickers, up to 200 in one request
+	def self.get_standard_quotes(ticker_symbol)
+		YahooFinance.get_standard_quotes(ticker_symbol)
+	end
+
+	#retrieves realtime additional company information
+	#can enter multiple tickers 
+	def self.get_extended_quotes(ticker_symbol)
+		YahooFinance.get_extended_quotes(ticker_symbol)
+	end
+
+
 
 end
